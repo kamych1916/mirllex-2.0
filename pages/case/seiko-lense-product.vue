234ded71f8f30ccc454f7f5564768645cae6ffc5
Rakhimov Kamol <34067263+kamych1916@users.noreply.github.com>
2021-07-28 01:02:57 +0500

    добавление кейса с очками - телеграм бот

---
+++ pages/case/seiko-lense-product.vue
@@ -0,0 +1,111 @@
+<template>
+  <main id="main" class="site-main bg-half-ring-right bg-half-ring-top pt-5">
+    <section id="page-content">
+      <div class="wrapper">
+        <div id="single">
+          <div class="row gutter-width-lg single-content">
+            <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+              <div class="img">
+                <div class="object-fit-cover">
+                  <img src="/images/seiko-background-1.jpg" />
+                </div>
+              </div>
+              <div class="img">
+                <div class="object-fit-cover">
+                  <img src="/images/seiko-background-2.jpg" />
+                </div>
+              </div>
+              <div class="img">
+                <div class="object-fit-cover">
+                  <img src="/images/seiko-background-3.jpg" />
+                </div>
+              </div>
+              <div class="img">
+                <div class="object-fit-cover">
+                  <img src="/images/seiko-background-4.jpg" />
+                </div>
+              </div>
+              <div class="img">
+                <div class="object-fit-cover">
+                  <img src="/images/seiko-background-5.jpg" />
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+              <div class="sticky">
+                <p class="date">
+                  с <span class="large">25</span>мая 2021 по
+                  <span class="large">30</span>июня 2021
+                </p>
+
+                <h1 class="small">Seiko Lense Product</h1>
+
+                <div class="description">
+                  <p>
+                    &laquo;Siko Lense&raquo; основана в&nbsp;2003&nbsp;г.
+                    Эксклюзивный представитель ведущих мировых оптических
+                    брендов на&nbsp;территории Казахстана. Мы&nbsp;представляем
+                    коллекции медицинских оправ, солнцезащитных очков
+                    и&nbsp;оптических линз мировых брендов.
+                  </p>
+
+                  <h5>Идея и задача</h5>
+
+                  <p>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus
+                    qui blanditiis praesentium voluptatum deleniti atque
+                    corrupti quos dolores et quas molestias excepturi sint
+                    occaecati cupiditate. At vero eos et accusamus et iusto odio
+                    dignissimos ducimus qui blanditiis praesentium voluptatum
+                    deleniti atque corrupti quos dolores et quas molestias
+                    excepturi sint occaecati cupiditate non provident, similique
+                    sunt in culpa.
+                  </p>
+
+                  <h5>Над проектом работал</h5>
+
+                  <div>Веб-мастер</div>
+                  <div class="mt-3">
+                    <b>Дмитрий Криничный</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Request class="my-5 py-5" />
+  </main>
+</template>
+
+<script>
+import WorksInsideData from "~/data/works-inside/worksInsideData.json";
+
+export default {
+  components: {},
+  data() {
+    return {
+      items: WorksInsideData.worksInsideData,
+    };
+  },
+  mounted: function () {
+    document.body.classList.add("single");
+    document.body.classList.add("single-portfolio");
+    document.body.classList.add("bg-fixed");
+    document.body.classList.add("bg-line");
+  },
+  beforeDestroy() {
+    document.body.classList.remove("single");
+    document.body.classList.remove("single-portfolio");
+    document.body.classList.remove("bg-fixed");
+    document.body.classList.remove("bg-line");
+  },
+  metaInfo: {
+    title: "Works inside | Oxer - Minimal Portfolio Vue JS Template",
+    titleTemplate: "%s",
+  },
+};
+</script>
